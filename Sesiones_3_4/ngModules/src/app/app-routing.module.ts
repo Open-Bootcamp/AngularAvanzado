77378b1fc6ac5aae7728f49509c2731669202ae7
RaulDeImagina <86669773+RaulDeImagina@users.noreply.github.com>
2022-01-25 12:02:53 +0100

    Add files via upload

---
+++ Sesiones_3_4/ngModules/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/pages/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./modules/pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./modules/pages/profile/profile.module').then(m => m.ProfileModule)
+  },
+  // Siempre el 404 se pondrá en el módulo de enrutado principal
+  {
+    path: '**',
+    loadChildren: () => import('./modules/pages/not-found/not-found.module').then(m => m.NotFoundModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
